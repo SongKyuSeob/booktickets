398ae77bcf5feaac8132ac0f0c63122c4f4f03bc
SongKyuSeob <sks5075@naver.com>
2022-07-31 18:10:46 +0900

    All done.

--- lib/screens/bottom_bar.dart
+++ lib/screens/bottom_bar.dart
@@ -1,4 +1,5 @@
 import 'package:booktickets/screens/home_screen.dart';
+import 'package:booktickets/screens/profile_screen.dart';
 import 'package:booktickets/screens/search_screen.dart';
 import 'package:booktickets/screens/ticket_screen.dart';
 import 'package:flutter/material.dart';
@@ -17,7 +18,7 @@ class _BottomBarState extends State<BottomBar> {
     const HomeScreen(),
     const SearchScreen(),
     const TicketScreen(),
-    const Text("Profile")
+    const ProfileScreen(),
   ];
 
   void _onItemTapped(int index){
